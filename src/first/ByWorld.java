b0b50620e010a4aaf4c8535381457d5875709db1
Mazen Kasher <mazenkasher17@gmail.com>
2022-08-23 11:31:32 -0400

    added ByWorld.java

---
+++ src/first/ByWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByWorld {
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 week of java");
+        System.out.println("1.5 weeks of git");
+    }
+}
